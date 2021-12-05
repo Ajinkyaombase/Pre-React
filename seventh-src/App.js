28230cc55bc1ba74290778d87a02710194732404
Ajinkyaombase <92512076+Ajinkyaombase@users.noreply.github.com>
2021-12-05 18:01:24 +0530

    Add files via upload

---
+++ seventh-src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Person from './Person';
+import Course from './Course';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Person/> */}
+      <Course/>
+    </div>
+  );
+}
+
+export default App;
