8cdd782b2fc315d8a0ee322e7b50d2548e767c5a
Ajinkyaombase <92512076+Ajinkyaombase@users.noreply.github.com>
2021-12-11 17:30:44 +0530

    Add files via upload

---
+++ UserPostHooksCurd/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import Person from './Person';
+import Course from './Course';
+import ClassComponentConversion from './ClassComponentConversion';
+import FunctionComponent from './FunctionComponent';
+import OnlineOrders from './OnlineOrders';
+import OfflineOrders from './OfflineOrders';
+import OnlineOrder2 from './OnlineOrder2';
+import OnlineOrderChild from './OnlineOrderChild';
+import UseEffectExample from './UseEffectExample';
+import HooksSample from './HooksSample';
+import RefParent from './RefParent';
+import PortalDemo from './PortalDemo';
+import ProfileDemo from './ProfileDemo';
+import HooksSampleUser from './HooksSampleUser';
+
+function App() {
+
+  return (
+    <div className="App" >
+      {/* <Person/>
+      <Course/> */}
+      {/* <ClassComponentConversion/>*/}
+      {/* <FunctionComponent/> */}
+      {/* <OnlineOrders/>
+      <OfflineOrders/> */}
+      {/* <OnlineOrder2/>
+      <OnlineOrderChild/> */}
+      {/* <UseEffectExample/> */}
+      {/* <HooksSample/> */}
+      <HooksSampleUser/>
+      {/* <RefParent/> */}
+      {/* I am inside App component
+      <PortalDemo/> */}
+      {/* <ProfileDemo/> */}
+    </div>
+  );
+}
+
+export default App;
